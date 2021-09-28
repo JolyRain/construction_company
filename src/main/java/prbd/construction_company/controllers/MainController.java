ba27366803a37e8fbd51a11b180037cbe677bcae
JolyRain <jolyrain@mail.ru>
2021-09-28 23:39:39 +0300

    aoaoaa

--- src/main/java/prbd/construction_company/controllers/MainController.java
+++ src/main/java/prbd/construction_company/controllers/MainController.java
@@ -14,6 +14,7 @@ public class MainController {
 
     @GetMapping("/")
     public String main(Model model) {
+        System.out.println("aadadsa");
         model.addAttribute("title", "Главная");
         return "index";
     }
