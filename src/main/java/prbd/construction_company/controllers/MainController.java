2d56dcd8018fd6a8377ef1b9520d28e58c6c4697
JolyRain <jolyrain@mail.ru>
2021-09-28 22:33:15 +0300

    added repositories for apartment and client

--- src/main/java/prbd/construction_company/controllers/MainController.java
+++ src/main/java/prbd/construction_company/controllers/MainController.java
@@ -12,17 +12,6 @@ import prbd.construction_company.repositories.HouseRep;
 @Controller
 public class MainController {
 
-    @Autowired
-    private CompanyRep companyRep;
 
-    @Autowired
-    private HouseRep houseRep;
 
-    @GetMapping("/")
-    public String test(Model model) {
-        ConstructionCompany company = companyRep.findById(1).get();
-        House house = new House(company, "address", "address");
-        houseRep.save(house);
-        return "home";
-    }
 }
--- src/main/resources/templates/greeting.ftl
+++
@@ -1,4 +0,0 @@
-<#import "blocks/base.ftl" as base>
-<@base.page>
-    <h1>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h1>
-</@base.page>
