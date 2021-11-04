6453b1437d0b2d3b65d45431718ee0efccf22fef
JolyRain <jolyrain@mail.ru>
2021-11-04 16:17:44 +0300

    handling exception when entry exists in db

--- src/main/java/prbd/construction_company/controller/CustomAdviceController.java
+++ src/main/java/prbd/construction_company/controller/CustomAdviceController.java
@@ -1,14 +1,11 @@
 package prbd.construction_company.controller;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import prbd.construction_company.dto.ApartmentDto;
 import prbd.construction_company.exception.NotFoundException;
-
-import javax.validation.Valid;
 
 @ControllerAdvice
 public class CustomAdviceController {
@@ -25,4 +22,9 @@ public class CustomAdviceController {
         return "error-page";
     }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    public String handleUniqueException(Model model) {
+        model.addAttribute("message", "Error! Entry already exists!");
+        return "error-page";
+    }
 }
